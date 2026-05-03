30cced155f5183b180522be20fe11e5191329ae3
Jessica Amorim <jessica_amorim@hotmail.com>
2026-05-03 19:39:50 +0100

    html e css das paginas index, sobre e receitas

---
+++ Assets/JS/main.js
@@ -0,0 +1,23 @@
+
+
+
+// ==== RECEITAS ====
+
+// -- Botão "Ver mais" --
+
+const receitas = document.querySelectorAll(".receita");
+const botao = document.getElementById("verMais");
+
+// esconder tudo depois do 6
+for (let i = 6; i < receitas.length; i++) {
+  receitas[i].style.display = "none";
+}
+
+// quando clicar no botão
+botao.onclick = function () {
+  for (let i = 6; i < receitas.length; i++) {
+    receitas[i].style.display = "block";
+  }
+
+  botao.style.display = "none";
+};
